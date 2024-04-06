c604420202ea9e08d4f115836eea1807a9fa43c2
Gsscego32 <111784522+Gsscego32@users.noreply.github.com>
2024-04-06 13:38:31 -0300

    Add files via upload

--- Metas de vida/botaos.js
+++ Metas de vida/botaos.js
@@ -1,14 +1,15 @@
 const botoes = document.querySelectorAll(".botoes");
-//Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
+//Declara quais elementos vao ser lidos. O script vai ler todos que tiverem a classe "botao".
 
 for(let i=0;i <botoes.length;i++){
-    //Variável "i" é igual a 0, mas enquanto essa variável for menor que a quantidade de elementos nessa lista, aumentamos o valor de "i", ou seja, i será a quantidade de botões.
+    //Variavel "i"e igual a 0, mas enquanto essa varivel for menor que a quantidade de elementos nessa lista, aumentamos o valor de "i", ou seja, i sera a quantidade de botoes.
     botoes[i].onclick = function() {
         for(let j=0;j<botoes.length;j++) {
         botoes[j].classList.remove("ativo");
         }
-        //Remove a classe "ativo" de todos os botões.
+        //Remove a classe "ativo" de todos os botoes.
         botoes[i].classList.add("ativo");
-        //Adiciona a classe ativo só no botão clicado.
+        //Adiciona a classe ativo so no botao clicado.
+
     }
 }
