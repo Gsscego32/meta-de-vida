1d62fec30a23631a85994a0df6b91c23e71ce010
Gsscego32 <111784522+Gsscego32@users.noreply.github.com>
2024-04-22 12:02:29 -0300

    Add files via upload

--- Metas de vida/botaos.js
+++ Metas de vida/botaos.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botoes");
+const texto= document.querySelectorAll(".abra-texto");
 //Declara quais elementos vao ser lidos. O script vai ler todos que tiverem a classe "botao".
 
 for(let i=0;i <botoes.length;i++){
@@ -6,9 +7,11 @@ for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function() {
         for(let j=0;j<botoes.length;j++) {
         botoes[j].classList.remove("ativo");
+        texto[j].classList.remove("ativo");
         }
         //Remove a classe "ativo" de todos os botoes.
         botoes[i].classList.add("ativo");
+        texto[i].classList.add("ativo");
         //Adiciona a classe ativo so no botao clicado.
 
     }
