7a1f100475018ac7f681fe6626f9d9da9424764b
Gsscego32 <111784522+Gsscego32@users.noreply.github.com>
2024-05-06 08:35:14 -0300

    Add files via upload

--- Metas de vida/botaos.js
+++ Metas de vida/botaos.js
@@ -3,6 +3,8 @@ const texto= document.querySelectorAll(".abra-Conteudo");
 const contadores= document.querySelectorAll(".contador");
 const TempoObjetivo1= new Date("2024-12-13T00:00:00");
 const tempoAtual= new Date;
+const TempoObjetivo2= new Date("2026-12-13T00:00:00")
+const TempoObjetivo3= new Date("2028-12-13T00:00:00")
 //Declara quais elementos vao ser lidos. O script vai ler todos que tiverem a classe "botao".
 
 for(let i=0;i <botoes.length;i++){
@@ -10,14 +12,50 @@ for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function() {
         for(let j=0;j<botoes.length;j++) {
         botoes[j].classList.remove("ativo");
-        texto[j].classList.remove("ativo");
-        }
+        texto[j].classList.remove("ativo")
+    
         //Remove a classe "ativo" de todos os botoes.
         botoes[i].classList.add("ativo");
         texto[i].classList.add("ativo");
         //Adiciona a classe ativo so no botao clicado.
-
+        }
     }
 }
 
-contadores[0].textContent= TempoObjetivo1 - tempoAtual;
+contadores[0].textContent=CalculaTempo(TempoObjetivo1);
+contadores[1].textContent=CalculaTempo(TempoObjetivo2);
+contadores[2].textContent=CalculaTempo(TempoObjetivo3);
+
+function CalculaTempo(TempoObjetivo){
+    let tempoAtual=new Date();
+    let tempoFinal=TempoObjetivo-tempoAtual;
+    let segundos=Math.floor(tempoFinal/1000);       
+    let minutos=Math.floor(segundos/60);
+    let horas=Math.floor(minutos/60);
+    let dias=Math.floor(horas/24);
+    let mes=Math.floor(dias/30);
+    let anos=Math.floor(mes/12);
+
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
+    dias%=30;
+    mes%=12;
+    return anos+"anos"+mes+"mes"+dias+"dias"+horas+"horas"+minutos+"minutos"+segundos+"segundos";
+
+
+}
+comecaCronometros();
+
+function comecaCronometros(){
+atualizaCronometro();
+setInterval(atualizaCronometro,1000);
+
+}
+
+function atualizaCronometro(){
+
+contadores[0].textContent = CalculaTempo(TempoObjetivo1);
+contadores[1].textContent = CalculaTempo(TempoObjetivo2);
+contadores[2].textContent = CalculaTempo(TempoObjetivo2);
+}
